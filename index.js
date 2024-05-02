5af7e41a9e42c4ee30226728ec6c22765e7e7390
Bishow08 <gdghamal522@gmail.com>
2024-05-02 22:11:51 +0545

    learned getting data

--- index.js
+++ index.js
@@ -9,19 +9,20 @@ app.get("/", (req, res)=>{
     res.json({ msg: "Hello  World! by Bishal"})
 });
 
-app.post("/:id", (req, res)=>{
+app.post("/post/:id", (req, res)=>{
     //client send the data
     res.json({ msg: "POST  World! by Bishal"})
 console.log({query:req.query,params: req.params, body:req.body});
 
 });
 
-app.put("/", (req, res)=>{
+app.put("/:id", (req, res)=>{
     //client send the data
+    console.log({query:req.query,params: req.params, body:req.body});
     res.json({ msg: "PUT  World! by Bishal"})
 });
 
-app.patch("/", (req, res)=>{
+app.patch("/:id", (req, res)=>{
     //client send the data
     res.json({ msg: "PATCH  World! by Bishal"})
 });
