ef55b2e14a0aa816f6f1c2ee60fc11d0418f822e
Bishow08 <gdghamal522@gmail.com>
2024-05-02 17:38:08 +0545

    learned routing

--- index.js
+++ index.js
@@ -4,10 +4,45 @@ const express = require("express");
 const app = express();
 const PORT = Number(process.env.PORT);
 
+app.use(express.json());
 app.get("/", (req, res)=>{
     res.json({ msg: "Hello  World! by Bishal"})
 });
 
+app.post("/:id", (req, res)=>{
+    //client send the data
+    res.json({ msg: "POST  World! by Bishal"})
+console.log({query:req.query,params: req.params, body:req.body});
+
+});
+
+app.put("/", (req, res)=>{
+    //client send the data
+    res.json({ msg: "PUT  World! by Bishal"})
+});
+
+app.patch("/", (req, res)=>{
+    //client send the data
+    res.json({ msg: "PATCH  World! by Bishal"})
+});
+
+app.delete("/", (req, res)=>{
+    res.json({ msg: "DELETE World! by Bishal"})
+});
+
+
 app.listen(PORT, () => {
     console.log(`Application is running on port ${PORT}`);
 });
+
+// const data = {
+//     name: "bishal",
+//     age: {dob: 1111},
+// }
+
+//destructure
+// during destructure = represents default value
+
+// const {name, age} = data;
+// console.log(name,age);
+
